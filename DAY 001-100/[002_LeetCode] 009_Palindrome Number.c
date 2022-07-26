28c0bee047e241566581ae3d32b8ed585c8163fd
chldygks <yonnsdev@gmail.com>
2022-07-27 04:32:29 +0900

    Day 002

---
+++ DAY 001-100/[002_LeetCode] 009_Palindrome Number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define bool int
+
+bool isPalindrome(int x);
+int main() { printf("%d", isPalindrome(-33333)); }
+
+// [Answer]
+bool isPalindrome(int x) {
+  int y = x;
+  int arr[10], i = 0;
+  if (x < 0)
+    return 0;
+  while (y > 0) {
+    arr[i] = y % 10;
+    y /= 10;
+    ;
+    i++;
+  }
+  for (int j = 0; j < i / 2; j++) {
+    if (arr[j] == arr[i - j - 1]) {
+      continue;
+    } else {
+      return 0;
+    }
+  }
+  return 1;
+}
